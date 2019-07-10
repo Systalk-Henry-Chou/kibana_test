06ec5480dfe0cb686c58f1f931e9f0a8add68173
henry <henry.chou@systalk-digital.com>
2019-07-10 15:02:41 +0800

    test

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,11 +12,11 @@ pipeline {
         stage('Test'){
             steps {
                 sh 'sh ./run.sh'
-                sh 'docker-compose stop'
             }
         }
         stage('Remove Image'){
             steps {
+                sh 'docker-compose stop'
                 sh 'docker-compose rm -y'
                 sh 'docker rmi flask/test:1 -y'
             }
--- run.sh
+++ run.sh
@@ -1,27 +1,25 @@
-@echo off
+#!/bin/bash
+# Generate Time
+TimeNow=$(date --iso-8601=seconds)
+echo $TimeNow
 
-rem Generate Time
-set date=%date:~0,4%%date:~5,2%%date:~8,2%
-if "%time:~0,2%" lss "10" (set hour=0%time:~1,1%) else (set hour=%time:~0,2%)
-set time=%hour%%time:~3,2%%time:~6,2%
-set d=%date%%time%
-echo Time Now: %d%
+# Build Path
+jmxPath=""
+jmeterPath="/opt/apache-jmeter-5.1.1/"
+echo $jmeterPath"lib/ext/cmdrunner-2.2.jar"
 
-rem Build Path
-set jmxPath=""
-set jmeterPath=$Jmeter_Home
+# Directory Build Time
+mkdir $TimeNow/
 
-rem Directory Build Time
-mkdir %jmxPath%/%d%
+# Run Jmeter
+jmeter -Jfilepath=$TimeNow -JthreadNum=10 -JrampUp=2 -Jcycles=1 -n -t TestPlan.jmx -l $TimeNow"/result.jtl" -e -o $TimeNow"/Report"
 
-rem Run Jmeter
-call jmeter -JfilePath="%jmxPath%/%d%" -JthreadNum=100 -JrampUp=25 -Jcycles=1 -n -t %jmxPath%/TestPlan.jmx -l %jmxPath%/result.jtl -e -o %jmxPath%/%d%/Report
+# Generator Monitor View
+echo $TimeNow"/result.jtl"
+java -Djava.awt.headless=true -jar $jmeterPath"lib/ext/cmdrunner-2.0.jar" --tool Reporter --generate-png $TimeNow"/ResponseTimesOverTime.png" --input-jtl $TimeNow"/result.jtl" --plugin-type ResponseTimesOverTime
+java -Djava.awt.headless=true -jar $jmeterPath"lib/ext/cmdrunner-2.0.jar" --tool Reporter --generate-png $TimeNow"/resource.png" --input-jtl $TimeNow"/resource.jtl" --plugin-type PerfMon 
 
-rem Generator Monitor View
-call java -jar %jmeterPath%/lib/ext/CMDRunner.jar --tool Reporter --generate-png %jmxPath%/%d%/ResponseTimesOverTime.png --input-jtl %jmxPath%/%d%/result.jtl --plugin-type ResponseTimesOverTime
-call java -jar %jmeterPath%/lib/ext/CMDRunner.jar --tool Reporter --generate-png %jmxPath%/%d%/resource.png --input-jtl %jmxPath%/%d%/resource.jtl --plugin-type PerfMon
+# Move Log
+mv jmeter.log $TimeNow
 
-rem Move Log
-move jmeter.log %jmxPath%/%d%
-
-rem pause
+exit 0
